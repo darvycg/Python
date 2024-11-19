315966e0ded9758e991eec4897d658863e362672
darvycg <darvy.ceron@gmail.com>
2024-11-19 17:28:07 -0600

    Added basic language examples

---
+++ IntroToPython/LanguageBasics.py
@@ -0,0 +1,46 @@
+is_game_over = False
+print(is_game_over)
+
+is_game_over = True
+print(is_game_over)
+
+is_game_over = 5 > 6
+print(is_game_over)
+
+num_lives = 5
+print(num_lives)
+
+percent_health = 0.5
+print(percent_health)
+
+player_name = 'Toxos'
+print(player_name)
+
+player_name = "Spoon"
+print(player_name)
+
+print(type(is_game_over))
+print(type(num_lives))
+print(type(percent_health))
+print(type(player_name))
+
+num_lives = 5
+str_num_lives = str(num_lives)
+print(type(num_lives))
+print(type(str_num_lives))
+
+print(bool(0))
+print(bool(0.1))
+print(bool("asdf"))
+print(bool("False"))
+
+# print(int("asdfasdf"))
+print(int("1"))
+print(int(0.5))
+print(int(False))
+print(int(True))
+
+print(float("1"))
+print(float(5))
+print(float(False))
+print(float(True))
